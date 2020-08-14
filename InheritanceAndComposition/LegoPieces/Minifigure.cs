c68374cef97404651599b147cc8cdb13a62708b6
Monique.Bass01 <monique.bass01@gmail.com>
2020-08-13 21:30:56 -0500

    Add project files.

---
+++ InheritanceAndComposition/LegoPieces/Minifigure.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InheritanceAndComposition.LegoPieces.Heads;
+using InheritanceAndComposition.LegoPieces.Legs;
+using InheritanceAndComposition.LegoPieces.Torsos;
+
+namespace InheritanceAndComposition
+{
+    class Minifigure
+    {
+        public string Name { get; }
+        public Head Head { get; }
+        public Torso Torso { get; }
+        public Legs Legs { get; }
+
+        public Minifigure(string name, Head head, Torso torso, Legs legs)
+        {
+            Name = name;
+            Head = head;
+            Torso = torso;
+            Legs = legs;
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"{Name} is saying hello:");
+            Torso.Wave();
+            Legs.Walk(20);
+            Head.Greet();
+        }
+
+    }
+}
---
+++ InheritanceAndComposition/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using InheritanceAndComposition.LegoPieces.Heads;
+using InheritanceAndComposition.LegoPieces.Legs;
+using InheritanceAndComposition.LegoPieces.Torsos;
+
+namespace InheritanceAndComposition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var astronautHead = new AstronautHead();
+            var pirateHead = new PirateHead();
+            var cowgirlHead = new CowgirlHead();
+
+            cowgirlHead.Greet();
+            cowgirlHead.HairFlip();
+
+            pirateHead.Greet();
+            pirateHead.Sing();
+
+            astronautHead.Greet();
+            astronautHead.Observe();
+
+            var heads = new List<Head>();
+            heads.Add(astronautHead);
+            heads.Add(pirateHead);
+            heads.Add(cowgirlHead);
+
+            Console.WriteLine(new string('=', 50));
+            foreach (var head in heads)
+            {
+                Console.WriteLine($"The {head.HairColor} haired, {head.EyeColor} eyed head is greeting you...");
+                head.Greet();
+            }
+
+            var farmerTorso = new FarmerTorso(Sex.Male);
+            var ninjaTorso = new NinjaTorso();
+
+            farmerTorso.PickUp("Goat in pajamas");
+            ninjaTorso.PickUp("Box");
+
+            var cyclistLegs = new CyclistLegs();
+            var countryMusicSingerLegs = new CountryMusicSingerLegs("Blue Jeans", false);
+
+
+            var astronaut = new Minifigure("Major Tom", astronautHead, ninjaTorso, countryMusicSingerLegs);
+            var cowgirl = new Minifigure("Jessie", cowgirlHead, farmerTorso, cyclistLegs);
+
+            cowgirl.SayHello();
+            astronaut.SayHello();
+        }
+    }
+}
